5cfbdc7459d8c02f9b5bc188ec6d46407e5d4435
Evie <ec22426@qmul.ac.uk>
2024-04-08 20:24:03 +0100

    changed settings for image file

--- expense_backend/expense_backend/settings.py
+++ expense_backend/expense_backend/settings.py
@@ -150,6 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+ #For uploading image files
+MEDIA_ROOT = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
