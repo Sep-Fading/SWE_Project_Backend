23619a32502a92b8600b999b503c6946659cefe2
Sepehr <main.sepehr@gmail.com>
2024-04-10 13:42:18 +0100

    model changes - sep

--- expense_backend/accounts/models.py
+++ expense_backend/accounts/models.py
@@ -101,7 +101,7 @@ class AccountModel(AbstractBaseUser, PermissionsMixin):
                                        choices=PERMISSION_CHOICES,
                                        default='EMPLOYEE')
 
-    flagged_password_change = models.BooleanField(default=False)
+    flagged_password_change = models.BooleanField(default=False, null=True)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -154,14 +154,14 @@ class EmployeeFormModel(models.Model):
     )
 
 
-    lineManagerID = models.CharField(max_length=100)
+    lineManagerID = models.CharField(max_length=100, null=True)
     dateMade = models.DateField(default=timezone.now().date())
     amount =  models.FloatField(default=0.0)
     currency = models.CharField(max_length=100)
     typeClaim = models.CharField(max_length=20,choices=CLAIM_TYPE,
                                        default='meal')
     description = models.CharField(max_length=500)
-    receipt = models.ImageField(null=True, blank=True, upload_to="receipts/")
+    receipt = models.ImageField(null=True, upload_to="receipts/")
     acknowledgement = models.BooleanField(default=False)
     status = models.CharField(max_length=20,choices=CLAIM_STATUS,
                                        default='PENDING')
